70cb6ec2df36358df3ba69b89fafea9acdc93cf7
penhoorn <penhoorn@gmail.com>
2022-12-22 13:35:54 +0100

    Parser arguments

--- pb-hd-tools.py
+++ pb-hd-tools.py
@@ -10,3 +10,14 @@ import pysam
 
 import hd-functions
 
+parser = argparse.ArgumentParser(
+    prog="pb-hd-tools",
+    description="A set of tools to count, filter and mask heteroduplex molecules and bases in PacBio HiFi data",
+    epilog="Thanks for using %(prog)s! :)",
+)
+
+parser.add_argument("infile")
+parser.add_argument("outfile")
+
+parser.add_argument("-f","--filter",action="store_true")
+parser.add_argument("-m","--mask",action="store_true")
