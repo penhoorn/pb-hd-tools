9bb69019cbe3c407aa082202a2483b056fff0d06
penhoorn <penhoorn@gmail.com>
2022-12-22 14:09:10 +0100

    Add output file extension check function

--- scripts/tools/filetype.py
+++ scripts/tools/filetype.py
@@ -1,7 +1,17 @@
+import sys
+
 def ifile(m):
   if m.lower().endswith('.bam'):
     return True
   elif m.lower().endswith('.fastq.gz'):
     return False
   else:
-    sys.exit("Input file must either have .bam or .fastq.gz extension")
+    sys.exit("ERROR: input file must either have .bam or .fastq.gz extension")
+
+def ofile(m):
+  if m.lower().endswith('.json'):
+    return True
+  elif m.lower().endswith('.csv'):
+    return False
+  else:
+    sys.exit("ERROR: output file must either have .json or .csv extension")
