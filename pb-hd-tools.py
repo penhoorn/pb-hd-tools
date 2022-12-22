7b26d844d875e7e583948737048dd706e70bf244
penhoorn <penhoorn@gmail.com>
2022-12-22 13:46:16 +0100

    Completed first version of CLI

--- pb-hd-tools.py
+++ pb-hd-tools.py
@@ -12,12 +12,14 @@ import pysam
 
 parser = argparse.ArgumentParser(
     prog="pb-hd-tools",
-    description="A set of tools to count, filter and mask heteroduplex molecules and bases in PacBio HiFi data",
+    description="A set of tools to count, filter and mask heteroduplex molecules and bases in PacBio CCS data",
     epilog="Thanks for using %(prog)s! :)",
 )
 
-parser.add_argument("infile")
-parser.add_argument("outfile")
+parser.add_argument("infile", type=str, help="Input file (either BAM or FASTQ.GZ)")
+parser.add_argument("outfile", type=str, help="Output file (either JSON or CSV)")
 
-parser.add_argument("-f","--filter",action="store_true")
-parser.add_argument("-m","--mask",action="store_true")
+parser.add_argument("-f","--filter",action="store_true",help="Filter out heteroduplex data (i.e., single-strand ccs reads")
+parser.add_argument("-m","--mask",action="store_true", help="Mask heteroduplex bases")
+
+args = parser.parse_args()
